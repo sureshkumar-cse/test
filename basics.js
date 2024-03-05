ab5e596259f0a616d1241a8b7ed206e2e81605f0
Suresh Kumar <sureshkumar@Sureshs-MacBook-Pro.local>
2024-03-05 22:13:25 +0530

    Added the JS Basics topics

---
+++ basics.js
@@ -0,0 +1,33 @@
+// What is JavaScript?
+// JavaScript => Scripting Language, run on browser, synchronous & single threaded, JIT(Just In Time) Compilation
+
+// What is the difference between let and var?
+// let -> Block Scope, Cannot redeclare block-scoped variable
+// var -> Function Scope, Redeclare, Hoisted
+
+/* function demo(){
+    var name = "Diya";
+}
+console.log(name); */
+
+/* let num = 10;
+let num = 20; */
+
+/* var num = 10;
+var num = 20;
+console.log(num); */
+
+/* console.log(fName);
+var fName = "Diya"; */
+
+/* var fName = "Diya";
+console.log(fName); */
+
+/* let fName = "Diya";
+console.log(fName); */
+
+/* console.log(fName);
+let fName = "Diya"; */
+
+// Hoisting follows the JavaScript Life-cycle
+
